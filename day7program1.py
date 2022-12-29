1557ce15a5acf3f2a69172f950b286501ba26ea7
POTHIREDDY JOSHNA <121159257+pothireddyjoshna19@users.noreply.github.com>
2022-12-29 12:15:52 +0530

    Add files via upload

---
+++ day7program1.py
@@ -0,0 +1,11 @@
+string='''
+Practice problems for List com pre hension in python.
+'''
+
+
+wordsLst=string.split(' ')
+print(wordsLst)
+wordsLst=[i.strip("\n") for i in wordsLst ]
+print(wordsLst)
+ans={i:len(i) for i in wordsLst }
+print(ans)
---
+++ day7program2.py
@@ -0,0 +1,12 @@
+string='''
+Practice problems for List com pre hension in python.
+'''
+
+
+wordsLst=string.split(' ')
+#print(wordsLst)
+wordsLst=[i.strip("\n") for i in wordsLst ]
+#print(wordsLst)
+
+ans={i:i[::-1] for i in wordsLst }
+print(ans)
